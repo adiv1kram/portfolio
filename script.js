fd59ed335f955351888fba7e05e58d5cbf22951f
adiv1kram <adityavikram250201@gmail.com>
2022-10-30 11:37:24 +0530

    daily update

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+// console.log("Script running...");
+// document.querySelector(".backArrow").style.display = "none";
+// document.querySelector(".ham").addEventListener("click", () => {
+//   document.querySelector(".sidebar").classList.toggle("sidebarGo");
+//   if (document.querySelector(".sidebar").classList.contains("sidebarGo")) {
+//     document.querySelector(".ham").style.display = "inline";
+//     document.querySelector(".backArrow").style.display = "none";
+//   } else {
+//     document.querySelector(".ham").style.display = "none";
+//     setTimeout(() => {
+//       document.querySelector(".backArrow").style.display = "inline";
+//     }, 300);
+//   }
+// });
 console.log("Script running...");
 document.querySelector(".backArrow").style.display = "none";
 document.querySelector(".ham").addEventListener("click", () => {
@@ -6,8 +20,8 @@ document.querySelector(".ham").addEventListener("click", () => {
     document.querySelector(".ham").style.display = "inline";
     document.querySelector(".backArrow").style.display = "none";
   } else {
-    document.querySelector(".ham").style.display = "none";
-    setTimeout(() =>{
-    document.querySelector(".backArrow").style.display = "inline"},300);
+    document.querySelector(".ham").style.display = "none"
+    setTimeout(() => {
+      document.querySelector(".backArrow").style.display = "inline"}, 300);
   }
 });

d1a83cff4a5e3d752db7949711fbfb0c62fc398b
adiv1kram <adityavikram250201@gmail.com>
2022-11-05 01:17:00 +0530

    daily update

--- script.js
+++ script.js
@@ -1,27 +1,20 @@
-// console.log("Script running...");
-// document.querySelector(".backArrow").style.display = "none";
-// document.querySelector(".ham").addEventListener("click", () => {
-//   document.querySelector(".sidebar").classList.toggle("sidebarGo");
-//   if (document.querySelector(".sidebar").classList.contains("sidebarGo")) {
-//     document.querySelector(".ham").style.display = "inline";
-//     document.querySelector(".backArrow").style.display = "none";
-//   } else {
-//     document.querySelector(".ham").style.display = "none";
-//     setTimeout(() => {
-//       document.querySelector(".backArrow").style.display = "inline";
-//     }, 300);
-//   }
-// });
+
 console.log("Script running...");
 document.querySelector(".backArrow").style.display = "none";
-document.querySelector(".ham").addEventListener("click", () => {
+document.querySelector(".ham").addEventListener("click", ()=> {
   document.querySelector(".sidebar").classList.toggle("sidebarGo");
-  if (document.querySelector(".sidebar").classList.contains("sidebarGo")) {
-    document.querySelector(".ham").style.display = "inline";
-    document.querySelector(".backArrow").style.display = "none";
-  } else {
-    document.querySelector(".ham").style.display = "none"
-    setTimeout(() => {
-      document.querySelector(".backArrow").style.display = "inline"}, 300);
-  }
-});
+  if (document.querySelector(".sidebar").classList.contains("sidebarGo"))
+  {
+        document.querySelector(".ham").style.display = 'inline'
+        document.querySelector(".backArrow").style.display = 'none'
+  } 
+  else 
+  {
+        document.querySelector(".ham").style.display = "none"
+        setTimeout(() => {
+            document.querySelector(".backArrow").style.display = "inline"
+        }, 300)
+    }
+})
+
+
